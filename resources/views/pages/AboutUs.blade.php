746cd4889dc0d3a8f8cf3df8ca3f182808befc81
sardor0990 <sardorzufarov98@gmail.com>
2020-04-25 21:39:17 +0300

    index-about_Sardor

---
+++ resources/views/pages/AboutUs.blade.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!----- Bootstrap link and scripts ---->
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <link rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script
+      src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <title>Restaurant</title>
+
+    <!-- about link-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+    <!--
+      CSS
+      ============================================= -->
+      <link rel="stylesheet" href="aboutpart/css/linearicons.css">
+      <link rel="stylesheet" href="aboutpart/css/font-awesome.min.css">
+      <link rel="stylesheet" href="aboutpart/css/bootstrap.css">
+      <link rel="stylesheet" href="aboutpart/css/magnific-popup.css">
+      <link rel="stylesheet" href="aboutpart/css/jquery-ui.css">        
+      <link rel="stylesheet" href="aboutpart/css/nice-select.css">              
+      <link rel="stylesheet" href="aboutpart/css/animate.min.css">
+      <link rel="stylesheet" href="aboutpart/css/owl.carousel.css">       
+      <link rel="stylesheet" href="aboutpart/css/main.css">
+      <style type="text/css">
+        .navbar{
+          height:65px !important;
+        }
+        .navbar a{
+          font-size: 16px!important;
+        }
+        #navbar-brand-t{
+          font-size:25px !important;
+        }
+        #social-media i{
+          color: black !important;
+          font-size: 16px !important
+        }
+      </style>
+  </head>
+  <body>
+    
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#" id="navbar-brand-t">Restaurant</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="{{URL('AboutUs')}}">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/menu">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/signin">Sign in</a>
+          </li>
+        </ul>
+        <div id="social-media">
+          <i class="fa fa-facebook-f"></i>
+          <i class="fa fa-instagram"></i>
+          <i class="fa fa-twitter"></i>
+          <i class="fa fa-youtube"></i>
+        </div>
+      </div>
+    </nav>
+
+
+
+<!-- start banner Area -->
+      <section class="about-banner relative">
+        <div class="overlay overlay-bg"></div>
+        <div class="container">       
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="about-content col-lg-12">
+              <h1 class="text-white">
+                About Us        
+              </h1> 
+              <p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="about.html"> About Us</a></p>
+            </div>  
+          </div>
+        </div>
+      </section>
+      <!-- End banner Area -->  
+
+      <!-- Start home-about Area -->
+      <section class="home-about-area section-gap">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 home-about-left">
+              <h1>About Our Story</h1>
+              <p>
+                Who are in extremely love with eco friendly system. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <a href="#" class="primary-btn">view full menu</a>
+            </div>
+            <div class="col-lg-6 home-about-right">
+              <img class="img-fluid" src="aboutpart/img/about-img.jpg" alt="">
+            </div>
+          </div>
+        </div>  
+      </section>
+      <!-- End home-about Area -->  
+
+      <!-- Start services Area -->
+      <section class="services-area pb-120">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-70 col-lg-8">
+              <div class="title text-center">
+                <h1 class="mb-10">What Kind of Services we Offer</h1>
+                <p>Who are in extremely love with eco friendly system.</p>
+              </div>
+            </div>
+          </div>            
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="single-service">
+                <div class="thumb">
+                  <img src="aboutpart/img/about/s1.jpg" alt="">
+                </div>
+                <a href="#">
+                  <h4>Bread Fruit Cheese Sandwich</h4>
+                </a>
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>                
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="single-service">
+                <div class="thumb">
+                  <img src="aboutpart/img/about/s2.jpg" alt="">
+                </div>
+                <a href="#">
+                  <h4>Beef Cutlet with Spring Onion</h4>
+                </a>  
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="single-service">
+                <div class="thumb">
+                  <img src="aboutpart/img/about/s3.jpg" alt="">
+                </div>
+                <a href="#">
+                  <h4>Meat with sauce & Vegetables</h4>
+                </a>  
+                <p>
+                  inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
+                </p>
+              </div>
+            </div>                        
+          </div>
+        </div>  
+      </section>
+      <!-- End services Area -->
+
+      <!-- Start review Area -->
+      <section class="review-area section-gap">
+        <div class="container">
+          <div class="row">
+            <div class="active-review-carusel">
+              <div class="single-review">
+                <img src="aboutpart/img/user.png" alt="">
+                <h4>Hulda Sutton</h4>
+                <div class="star">
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>                
+                </div>  
+                <p>
+                  “Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.”
+                </p>
+              </div>
+              <div class="single-review">
+                <img src="aboutpart/img/user.png" alt="">
+                <h4>Hulda Sutton</h4>
+                <div class="star">
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>                
+                </div>  
+                <p>
+                  “Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.”
+                </p>
+              </div>  
+              <div class="single-review">
+                <img src="aboutpart/img/user.png" alt="">
+                <h4>Hulda Sutton</h4>
+                <div class="star">
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>                
+                </div>  
+                <p>
+                  “Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.”
+                </p>
+              </div>
+              <div class="single-review">
+                <img src="aboutpart/img/user.png" alt="">
+                <h4>Hulda Sutton</h4>
+                <div class="star">
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>                
+                </div>  
+                <p>
+                  “Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.”
+                </p>
+              </div>                            
+            </div>
+          </div>
+        </div>  
+      </section>
+      <!-- End review Area -->        
+              <!-- Footer -->
+              <footer class="page-footer font-small unique-color-dark pt-4">
+
+             <!-- Footer Elements -->
+               <div class="container">
+
+               <!-- Call to action -->
+               <ul class="list-unstyled list-inline text-center py-2">
+               <li class="list-inline-item">
+                  <h5 class="mb-1 text-white">Register for free</h5>
+               </li>
+               <li class="list-inline-item">
+                <a href="#!" class="btn btn-outline-white btn-rounded">Sign up!</a>
+               </li>
+                 </ul>
+                <!-- Call to action -->
+
+                </div>
+                    <!-- Footer Elements -->
+
+                  <!-- Copyright -->
+                <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                 <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                   </div>
+                           <!-- Copyright -->
+
+                       </footer>
+                        <!-- Footer -->
+
+   
+    
+    
+   
+
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+
+
+    <script src="aboutpart/js/vendor/jquery-2.2.4.min.js"></script>
+      <script src="aboutpart/js/popper.min.js"></script>
+      <script src="aboutpart/js/vendor/bootstrap.min.js"></script>      
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>   
+      <script src="aboutpart/js/jquery-ui.js"></script>         
+        <script src="aboutpart/js/easing.min.js"></script>      
+      <script src="aboutpart/js/hoverIntent.js"></script>
+      <script src="aboutpart/js/superfish.min.js"></script> 
+      <script src="aboutpart/js/jquery.ajaxchimp.min.js"></script>
+      <script src="aboutpart/js/jquery.magnific-popup.min.js"></script>           
+      <script src="aboutpart/js/jquery.nice-select.min.js"></script>          
+      <script src="aboutpart/js/owl.carousel.min.js"></script>      
+            <script src="aboutpart/js/isotope.pkgd.min.js"></script>                
+      <script src="aboutpart/js/mail-script.js"></script> 
+      <script src="aboutpart/js/main.js"></script>  
+  </body>
+</html>
+
