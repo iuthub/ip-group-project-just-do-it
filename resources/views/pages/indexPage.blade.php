61b4078784a4c09f655b2d763d427154fabe3beb
davrontb <davron.tursunov.2014@mail.ru>
2020-04-25 15:33:09 +0500

    index break into parts

--- resources/views/pages/indexPage.blade.php
+++ resources/views/pages/indexPage.blade.php
@@ -27,25 +27,10 @@
     @include("partials.navbar")
 
     @include("partials.body")
-    <footer>
-      <div class="container conclusion">
-       <div class="context-footer">
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam quibusdam sit corporis? At officia aspernatur illo laboriosam nostrum aliquam, consectetur maxime debitis, similique magnam eligendi dolore autem? Minima ad corrupti vero modi quo voluptatem ducimus repudiandae veniam corporis eius iste facilis quae temporibus reprehenderit nobis laborum cupiditate, alias placeat hic?</p>
-       </div>
-       <div class="context-footer">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam quibusdam sit corporis? At officia aspernatur illo laboriosam nostrum aliquam, consectetur maxime debitis, similique magnam eligendi dolore autem? Minima ad corrupti vero modi quo voluptatem ducimus repudiandae veniam corporis eius iste facilis quae temporibus reprehenderit nobis laborum cupiditate, alias placeat hic?</p>
-      </div>
-      <div class="context-footer">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam quibusdam sit corporis? At officia aspernatur illo laboriosam nostrum aliquam, consectetur maxime debitis, similique magnam eligendi dolore autem? Minima ad corrupti vero modi quo voluptatem ducimus repudiandae veniam corporis eius iste facilis quae temporibus reprehenderit nobis laborum cupiditate, alias placeat hic?</p>
-      </div>
-      <div class="context-footer">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam quibusdam sit corporis? At officia aspernatur illo laboriosam nostrum aliquam, consectetur maxime debitis, similique magnam eligendi dolore autem? Minima ad corrupti vero modi quo voluptatem ducimus repudiandae veniam corporis eius iste facilis quae temporibus reprehenderit nobis laborum cupiditate, alias placeat hic?</p>
-      </div>
-    </div>
-    </footer>
-    <div class="moveUp">
-      <span><i class="fa fa-arrow-up"></i></span>
-    </div>
+    
+    @include("partials.footer")
+
+
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
     <script type="text/javascript" src="js/popper.min.js"></script>
