c42ac0a44cde619c376dafdaab47b0f90fcc8cfe
sardor0990 <sardorzufarov98@gmail.com>
2020-05-05 00:29:56 +0300

    book response modified

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -23,6 +23,9 @@ class ConfirmPasswordController extends Controller
 
     /**
      * Where to redirect users when the intended url fails.
+     InputField::make('project.name')
+      ->type('text')
+     ->title('Название проекта')
      *
      * @var string
      */
